6da2f5a85c38d67c69c3bb757aefc643752a0c25
samimi-electronicst <samimi.electronics@gmail.com>
2020-10-23 17:09:32 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function main() {
+	console.log('main() executing')
+	const canvas = document.querySelector('#glCanvas')
+	
+	// Init Gl context
+	const gl = canvas.getContext('webgl')
+
+	// Continue if webgl is available
+	if (gl === null) {
+		alert('Unable to initialize WebGL.')
+		return
+	}
+
+
+	// set clear color to black, fully opaque
+	gl.clearColor(0.84, 0.25, 0.56, 1)
+
+	// Clear the color buffer with specified clear color
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+}
+
+window.onload = main
